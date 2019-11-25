8ff55978d4d57dbfa372a2aa07f91ed0199e6a24
Cook <jecook@vlsi17.bu.edu>
2019-11-25 15:55:29 -0500

    Added new prototypes to pokemon header file

--- Pokemon.h
+++ Pokemon.h
@@ -2,6 +2,7 @@
 #define POKEMON_H
 #include "PokemonCenter.h"
 #include "PokemonGym.h"
+#include "Rival.h"
 using namespace std;
 
 enum PokemonStates {
@@ -13,7 +14,13 @@ enum PokemonStates {
     MOVING_TO_GYM = 5,
     MOVING_TO_CENTER = 6,
     TRAINING_IN_GYM = 7,
-    RECOVERING_STAMINA = 8
+    RECOVERING_STAMINA = 8,
+
+    // new states added in PA4
+    IN_ARENA = 9,
+    MOVING_TO_ARENA = 10,
+    BATTLE = 11,
+    FAINTED = 12,
 };
 
 class Pokemon: public GameObject
@@ -22,6 +29,7 @@ class Pokemon: public GameObject
     Pokemon();
     Pokemon(char in_code);
     Pokemon(string in_name, int in_id, char in_code, unsigned int in_speed, Point2D in_loc);
+    Pokemon(string in_name, double speed, double hp, double phys_dmg, double magic_dmg, double def, int in_id, char in_code, Point2D in_loc); // PA4 added constructor
     virtual ~Pokemon();
 
     void StartMoving(Point2D dest);
@@ -35,9 +43,25 @@ class Pokemon: public GameObject
     bool Update();
     void ShowStatus();
 
+    // new public member functions from PA4
+    bool IsAlive();
+    void TakeHit(double physical_damage, double magical_damage, double defense);
+    void ReadyBattle(Rival* in_target);
+    bool StartBattle();
+
     protected:
     bool UpdateLocation();
     void SetupDestination(Point2D dest);
+
+    // new protected members from PA4
+    double health;
+    double store_health;
+    double physical_damage;
+    double magical_damage;
+    double defense;
+    Rival* target;
+    bool is_in_arena;
+    BattleArena* current_arena;
 
     private:
     double speed;
